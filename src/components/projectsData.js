008ca15b625cf9f5c7dd2b3d88394d9b1773eeaa
Jia XU <xujia118@hotmail.com>
2024-07-13 10:25:24 -0700

    Added voice2task

--- src/components/projectsData.js
+++ src/components/projectsData.js
@@ -1,15 +1,21 @@
 import lotusgarden from "../assets/lotusgarden.png";
 import bonnati from "../assets/bonnati.png";
 import tradingrobot from "../assets/tradingrobot.png";
+import voice2Task from "../assets/voice2Task.png";
 
 export const projectsData = [
   {
-    name: "Park Website",
-    image: lotusgarden,
+    name: "Voice2Task",
+    image: voice2Task,
     description:
-      "A beautiful website for a famous garden in China dedicated to foreign visitors",
-    technologies: ["React", "HTML/CSS", "Responsive Design", "Accessibility"],
-    url: "https://www.lotusgardenhz.com",
+      "A smart sales assistant that summarizes phone call and generates action items",
+    technologies: [
+      "AWS SDK: S3, Transcribe, RDS MySQL",
+      "Docker",
+      "Ngnix",
+      "ClaudeAI",
+    ],
+    url: "https://github.com/Xujia118/Voice2Task",
   },
   {
     name: "Restaurant Client Order System",
@@ -20,14 +26,22 @@ export const projectsData = [
     url: "https://dine-dash.up.railway.app/",
   },
   {
+    name: "Park Website",
+    image: lotusgarden,
+    description:
+      "A beautiful website for a famous garden in China dedicated to foreign visitors",
+    technologies: ["React", "HTML/CSS", "Responsive Design", "Accessibility"],
+    url: "https://www.lotusgardenhz.com",
+  },
+  {
     name: "Stocks Trading Robot",
     image: tradingrobot,
     description:
-      "Developed an autonomous stock trading robot in Python, delivering exceptional returns",
+      "An autonomous stock trading robot delivering exceptional returns",
     technologies: [
       "Python",
       "Objected-oriented programming",
-      "Data structures and algorithms",
+      "AWS EC2",
       "Trading strategies",
     ],
     url: "https://github.com/Xujia118/Trading-Bot",
