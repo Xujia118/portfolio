43a3e5cdf3056dcbbf3d9c3ca4690c06c1a28249
Jia XU <xujia118@hotmail.com>
2024-07-13 21:12:45 -0700

    Updated skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,10 +4,10 @@ function Skills() {
   return (
     <section id="skills">
       <h3>MY SKILLS -</h3>
-      <p>Python • C++ • JavaScript • TypeScript • Java</p>
+      <p>Python • C++ • JavaScript • TypeScript • Java • Swift • Linux</p>
       <p>
-        Node • Express • React • SQL/MongoDB • HTML/CSS • RESTful APIs •
-        Responsive Design
+        Node • Express • React • SwiftUI • SQL/MongoDB • HTML/CSS • RESTful APIs
+        • Docker • Kubernetes • Nginx • Responsive Design
       </p>
       <p>Certified AWS Solutions Architect</p>
       <p>Git • Docker</p>
